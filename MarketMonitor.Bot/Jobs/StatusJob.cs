c6aa5fbec7bb86bd945ec7685345023320c4b244
Derek Alsop <livewire192@gmail.com>
2025-03-14 02:54:53 -0700

    Add README and add new statuses

--- MarketMonitor.Bot/Jobs/StatusJob.cs
+++ MarketMonitor.Bot/Jobs/StatusJob.cs
@@ -1,22 +1,47 @@
-﻿using Discord.WebSocket;
+﻿using Discord;
+using Discord.WebSocket;
 using MarketMonitor.Bot.Services;
 using MarketMonitor.Database;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
 namespace MarketMonitor.Bot.Jobs;
 
+public struct Status(string text, ActivityType type = ActivityType.CustomStatus)
+{
+    public ActivityType Type { get; set; } = type;
+    public string Text { get; set; } = text;
+}
+
 public class StatusJob(DiscordSocketClient client, PrometheusService stats, DatabaseContext db)
 {
     private int lastStatus;
-    private readonly string[] statuses = ["/help", "eris.gg"];
+
+    private readonly Status[] statuses =
+    [
+        new("/character setup"), new("{characters}", ActivityType.Watching), new("/retainer setup"), new("{retainers}", ActivityType.Watching), new("eris.gg")
+    ];
 
     public async Task SetStatus()
     {
         try
         {
-            await client.SetCustomStatusAsync(statuses[lastStatus]);
+            var status = statuses[lastStatus];
+            if (status.Text.Contains("{characters}"))
+            {
+                var amount = await db.Characters.Where(c => c.IsVerified).CountAsync();
+                await client.SetGameAsync($"{amount:N0} {"character".Quantize(amount)}", type: status.Type);
+            }
+            else if (status.Text.Contains("{retainers}"))
+            {
+                var amount = await db.Retainers.Where(c => c.IsVerified).CountAsync();
+                await client.SetGameAsync($"{amount:N0} {"retainer".Quantize(amount)}", type: status.Type);
+            }
+            else
+            {
+                await client.SetGameAsync(status.Text, type: status.Type);
+            }
+
             lastStatus++;
             if (lastStatus == statuses.Length) lastStatus = 0;
             stats.Latency.Set(client.Latency);
---
+++ README.md
@@ -0,0 +1,59 @@
+﻿<h1 align="center">Market Monitor</h1>
+
+<h4 align="center">An easy-to-use bot that notifies you of undercutting in the markets of Eorzea<br/>and tracks your sale and purchase history.</h4>
+
+<p align="center">
+  <a href="https://discord.gg/66dp9gxMZx">
+    <img src="https://discordapp.com/api/guilds/918704583717572639/widget.png?style=shield" alt="Discord Server">
+  </a>
+  <a href="https://github.com/Azorant/MarketMonitor/actions">
+    <img src="https://img.shields.io/github/actions/workflow/status/Azorant/MarketMonitor/docker-publish.yml?label=Build" alt="GitHub Actions">
+  </a>
+</p>
+
+# Getting Started
+Install [Docker](https://docs.docker.com/engine/install/) and [Docker Compose](https://docs.docker.com/compose/install/)
+
+Create a `docker-compose.yml` with the following content:
+
+```yaml
+version: '3.8'
+
+services:
+  marketmonitor:
+    image: ghcr.io/azorant/marketmonitor:latest
+    container_name: marketmonitor
+    restart: unless-stopped
+    environment:
+      - DISCORD_INVITE=server invite
+      - REDIS=redis
+      - LOG_CHANNEL=channel for logging status updates
+      - GUILD_CHANNEL=channel for logging guild join/leave
+      - TOKEN=bot token
+      - DB=server=maria;user=root;password=Market;database=marketmonitor
+    ports:
+      - 3400:3400
+    depends_on:
+      maria:
+        condition: service_started
+      redis:
+        condition: service_started
+  maria:
+    image: mariadb:latest
+    container_name: maria
+    restart: unless-stopped
+    environment:
+      - MARIADB_ROOT_PASSWORD=Market
+    expose:
+      - 3306
+  redis:
+    image: redis:alpine
+    container_name: redis
+    restart: unless-stopped
+    expose:
+      - 6379
+```
+
+Run `docker compose up -d` to startup MarketMonitor, MariaDB and Redis.
+
+That's all!
